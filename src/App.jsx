45b5175cf64950d26f04ee27b50bcc944b866417
Jiuk Choi <58076159+optimalMachine@users.noreply.github.com>
2022-08-31 20:52:12 +0000

    Initiate project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import * as React from 'react';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  )
+}
